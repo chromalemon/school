65e0de685ebf332367ed3ba914bf1e83752b027b
Nathaniel Melnichenko <19nmelnichenko@chelsea-academy.org>
2025-11-06 09:09:05 +0000

    Refined stack

--- DSA/stack.py
+++ DSA/stack.py
@@ -1,41 +1,46 @@
 class Stack:
     def __init__(self):
-        self.stack = []
+        self.stack = [0 for i in range(11)]
         self.pointer = -1
 
     def push(self, val):
         if self.pointer <= 10:
-            self.stack.append(val)
             self.pointer += 1
+            self.stack[self.pointer] = val
             return 1
         else:
             return 0 
         
     def pop(self):
         if self.pointer >= 0:
-            self.stack.pop(-1)
             self.pointer -= 1
-            return 1
+            return self.stack[self.pointer + 1]
         else:
-            return 0 
+            return None
         
     def peek(self):
         if self.pointer >= 0:
-            return self.stack[-1]
+            return self.stack[self.pointer]
         else:
-            return 0 
+            return None
         
     def get_stack(self):
-        return self.stack
+        stack = []
+        for i in range(self.pointer + 1):
+            stack.append(self.stack[i])
+        return stack
     
     def size(self):
-        return len(self.stack)
+        return self.pointer + 1
+    
+    def is_empty(self):
+        return self.pointer == -1
         
 stack1 = Stack()
 stack1.push(3)
 print(stack1.get_stack())
 stack1.push(4)
 print(stack1.get_stack())
-stack1.peek()
+print(stack1.peek())
 stack1.pop()
 print(stack1.get_stack())
